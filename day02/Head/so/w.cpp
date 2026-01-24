964679207fdde9a749640b7434a37473d550b7eb
Chen <czpct123@163.com>
2026-01-24 22:18:47 +0800

    无法编译多个C++文件，只学会添加本地头文件，还需要学习vscode的编译多个C++文件的配置方法

--- day02/Head/so/w.cpp
+++ day02/Head/so/w.cpp
@@ -1,1 +1,3 @@
+
+
 #include "w.h"
---
+++ day02/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include "head/swap.h"
+using namespace std;
+
+int main ()
+{
+    int a = 1,b =2;
+    cout<<"a:"<<a<<"b:"<<b<<endl;
+    swap(a,b);
+    cout<<"a:"<<a<<"b:"<<b<<endl;
+    return 0;
+}
